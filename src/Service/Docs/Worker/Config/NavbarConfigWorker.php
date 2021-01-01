fa91847e285da94fd7cd641d500919709f00018c
natepage <nathan.page86@gmail.com>
2021-01-02 00:13:56 +1100

    Add projects dropdown to navbar

---
+++ src/Service/Docs/Worker/Config/NavbarConfigWorker.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Docs\Worker\Config;
+
+final class NavbarConfigWorker extends AbstractDocsConfigWorker
+{
+    /**
+     * @param mixed[] $config
+     * @param \App\Service\Project\Contract\ProjectInterface[] $projects
+     *
+     * @return mixed[]
+     */
+    public function work(array $config, array $projects): array
+    {
+        $projectItems = [];
+
+        foreach ($projects as $project) {
+            $projectItems[] = [
+                'text' => $project->getName(),
+                'link' => \sprintf('packages/%s/', $project->getSlug()),
+            ];
+        }
+
+        $config['themeConfig']['nav'] = [
+            [
+                'text' => 'Projects',
+                'items' => $projectItems,
+            ],
+        ];
+
+        return $config;
+    }
+}
