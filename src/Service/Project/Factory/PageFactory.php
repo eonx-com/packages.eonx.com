130676ec407d3346b15d36e2443b267c490b9aee
natepage <nathan.page86@gmail.com>
2021-01-27 11:20:04 +1100

    Handle syntax error in yaml config in page

--- src/Service/Project/Factory/PageFactory.php
+++ src/Service/Project/Factory/PageFactory.php
@@ -8,6 +8,7 @@ use App\Service\Project\Contract\PageInterface;
 use App\Service\Project\Contract\ProjectInterface;
 use App\Service\Project\Dto\Page;
 use Nette\Utils\Strings;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Yaml\Yaml;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -23,10 +24,17 @@ final class PageFactory implements PageFactoryInterface
      */
     private const SLASHES_WITH_SPACES_PATTERN = '(?:---eonx_docs---[\s]*[\r\n]+)';
 
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function createFromFileInfo(SmartFileInfo $fileInfo, ProjectInterface $project): ?PageInterface
     {
-        $matches = Strings::match($fileInfo->getContents(), self::CONFIG_CONTENT_PATTERN);
-        $config = isset($matches['config']) ? Yaml::parse($matches['config']) : [];
+        $matches = Strings::match($fileInfo->getContents(), self::CONFIG_CONTENT_PATTERN) ?? [];
+        $config = $this->getConfig($matches);
         $contents = $matches['content'] ?? $fileInfo->getContents();
 
         if (isset($config['ignore'])) {
@@ -41,6 +49,31 @@ final class PageFactory implements PageFactoryInterface
             $config['weight'] ?? null,
             $config['title'] ?? null
         );
+    }
+
+    /**
+     * @param mixed[] $matches
+     *
+     * @return mixed[]
+     */
+    private function getConfig(ProjectInterface $project, SmartFileInfo $page, array $matches): array
+    {
+        if (isset($matches['config']) === false) {
+            return [];
+        }
+
+        try {
+            return Yaml::parse($matches['config']);
+        } catch (\Throwable $throwable) {
+            $this->logger->error(\sprintf(
+                'Error while parsing config in Project "%s", Page "%s": %s',
+                $project->getName(),
+                $page->getFilename(),
+                $throwable->getMessage()
+            ));
+
+            return [];
+        }
     }
 
     private function getPathInProject(SmartFileInfo $fileInfo, ProjectInterface $project): string
